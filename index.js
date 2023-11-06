124b6c0fb52da4e2127301caada7ff4cc5e9afa5
probeshnath <probeshn@gmail.com>
2023-11-06 15:10:51 +0600

    create jobs added

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+const cors = require('cors')
+require('dotenv').config()
 const port = process.env.PORT || 5000;
+
+
+// middlewarw 
+app.use(cors())
+app.use(express.json())
+
+
+const uri = process.env.MONGODB_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const jobs = client.db("trust").collection("jobs")
+
+    app.post("/jobs", async(req,res)=>{
+       const job = req.body;
+       const result = await jobs.insertOne(job)
+       console.log(result)
+       res.send(result)
+    })
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 
 app.get("/",(req,res)=>{
     res.send("Home route")
 })
+
 
 
 app.listen((port), ()=>{
